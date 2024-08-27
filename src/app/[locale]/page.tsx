cef89355be08aa11fcf4cd6c17589e4d598d3e88
Mustafa <aslanov7509@gmail.com>
2024-08-27 14:46:04 +0400

    About section is completed

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -6,36 +6,22 @@ import Empowering from "@/components/Pages/Home/Empowering";
 import Features from "@/components/Pages/Home/Features";
 import Services from "@/components/Cards/ServicesCard";
 import styles from "@/assets/css/style.module.css";
+import About from "@/components/Pages/Home/About";
 export default function Home() {
   const t = useTranslations("HomePage");
-
-
-  // useEffect(() => {
-  //   // Function to adjust border height
-  //   function adjustBorderHeight() {
-  //     document.body.style.borderRight = '5px solid black'; // Apply the border style
-  //     document.body.style.height = window.innerHeight + 'px'; // Adjust the body's height
-  //   }
-
-  //   // Adjust on load
-  //   adjustBorderHeight();
-
-  //   // Adjust on window resize
-  //   window.addEventListener('resize', adjustBorderHeight);
-
-  //   // Cleanup event listener on component unmount
-  //   return () => {
-  //     window.removeEventListener('resize', adjustBorderHeight);
-  //   };
-  // }, []);
 
   return (
     <>
       <h1>{t("title")}</h1>
 
-      <section>
+      <section >
         <div className="container">
           <Empowering />
+        </div>
+      </section>
+      <section>
+        <div className="container">
+          <About />
         </div>
       </section>
       <section>
