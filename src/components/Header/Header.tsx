fce205ac4ff85b768262e8c74e26b8d2069cee11
hemid000 <hmid.yusifli04@mail.ru>
2024-08-29 15:13:11 +0400

    changes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -21,6 +21,21 @@ const Header: React.FC = () => {
           </ul>
         </div>
         <div className={`${styles.right}`}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="36"
+            height="36"
+            viewBox="0 0 36 36"
+            fill="none"
+          >
+            <path
+              d="M6.004 10H30.004M6 18H29.994M6.004 26H29.994"
+              stroke="#4E4E4E"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
           <ul className="flex items-center gap-6">
             <li>About us</li>
             <li>Contact</li>
