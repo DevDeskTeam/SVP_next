bb50809e362f2e64cd05d343dd54219a370d4f34
hemid000 <hmid.yusifli04@mail.ru>
2024-09-30 16:02:25 +0400

    language updated

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -9,7 +9,7 @@ import CustomerCard from "@/components/Cards/CustomerCard";
 import SolutionSect from "@/components/Cards/SolutionSect";
 import About from "@/components/Pages/Home/About";
 import Trading from "@/components/Pages/Home/Trading";
-import Servicesmock from "../../../mock/HomeServices"
+// import Servicesmock from "../../../mock/HomeServices"
 export default function Home() {
   const t = useTranslations("HomePage");
 
