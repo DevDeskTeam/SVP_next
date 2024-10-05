81c9e18561e04642f924c2867c0040cb14044eae
Mustafa <aslanov7509@gmail.com>
2024-10-05 17:53:33 +0400

    Home page is updated

--- src/components/Swiper/home_swiper.tsx
+++ src/components/Swiper/home_swiper.tsx
@@ -1,6 +1,10 @@
 'use client'
 import '@/assets/css/home/swiper_home.css'
 import React, { useRef, useState } from 'react';
+import { useTranslations } from 'next-intl';
+import { useServices } from "../../../mock/HomeServices";
+
+
 // Import Swiper React components
 
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -13,46 +17,30 @@ import 'swiper/css/navigation';
 
 // import required modules
 import { Navigation } from 'swiper/modules';
-``
+
 export default function App() {
+    const t = useTranslations('HomePageServices')
+    const services = useServices();
     return (
         <>
             <Swiper
                 navigation={true}
                 modules={[Navigation]}
                 className="mySwiper"
-                slidesPerView={1} 
+                slidesPerView={1}
             // spaceBetween={50}
 
             >
-                <SwiperSlide>
-                    <div className='services'>
-                        <h1>Transportation services</h1>
-                        <p>Figma ipsum component variant main layer. Slice list scale figjam prototype slice reesizing. </p>
-                        <button>Learn More</button>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className='services'>
-                        <h1>Transportation services</h1>
-                        <p>Figma ipsum component variant main layer. Slice list scale figjam prototype slice reesizing. </p>
-                        <button>Learn More</button>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className='services'>
-                        <h1>Transportation services</h1>
-                        <p>Figma ipsum component variant main layer. Slice list scale figjam prototype slice reesizing. </p>
-                        <button>Learn More</button>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className='services'>
-                        <h1>Transportation services</h1>
-                        <p>Figma ipsum component variant main layer. Slice list scale figjam prototype slice reesizing. </p>
-                        <button>Learn More</button>
-                    </div>
-                </SwiperSlide>
+                {services.map((el) => (
+                    <SwiperSlide>
+                        <div className='services'>
+                            <h1>{el.title}</h1>
+                            <p>{el.description}</p>
+                            <button>{t('learnMore')}</button>
+                        </div>
+                    </SwiperSlide>
+                ))}
+
                 {/* <div className='rectangular'>  aKM
                 </div> */}
             </Swiper>
