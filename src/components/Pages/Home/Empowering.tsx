a41f6fb6da36076129c11d4e2387834a8c24b554
hemid000 <hmid.yusifli04@mail.ru>
2024-10-05 15:40:45 +0400

    changes

--- src/components/Pages/Home/Empowering.tsx
+++ src/components/Pages/Home/Empowering.tsx
@@ -4,16 +4,16 @@ import styles from "@/assets/css/home/empowering.module.css";
 import Background from "@/assets/images/Hero.png";
 import Swiper_Home from "@/components/Swiper/home_swiper"
 import '@fontsource/open-sans';
+import { useTranslations } from 'next-intl';
 
 const Empowering = () => {
+  const t = useTranslations('HomePage')
   return (
     <div>
       <div className={`${styles.banner}`}>
         <Image src={Background} alt="background" />
         <div className={`${styles.text}`}>
-          <p>Empowering
-            your global trade
-            journey</p>
+          <p> {t('empowering')} </p>
         </div>
         <Swiper_Home />
         {/* <div className={`${styles.rectangular}`}></div> */}
