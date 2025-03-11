a83c9c6c6aa5c289d69fea761d609326e770bfac
hemid000 <hmid.yusifli04@mail.ru>
2025-03-12 00:19:17 +0400

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // comment out this line
+  // output: "export", // Comment out this line to avoid the conflict with middleware
 };
 
 export default withNextIntl(nextConfig);
